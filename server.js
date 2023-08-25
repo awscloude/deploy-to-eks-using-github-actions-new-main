9ff0e948d177a543aa073be7617e1a900affdb11
awscloude <97865454+awscloude@users.noreply.github.com>
2023-08-25 16:18:08 +0600

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from from Primus Learning, this was awesome for version three(333)';
+  const msg = 'Hello from from Primus Learning, this was awesome for version four(444)';
   res.send(getPage(msg));
 });
 
